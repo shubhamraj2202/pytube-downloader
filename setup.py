ff7a1d1b379594d139164fe3d33621269e48d1a6
Shubham Raj <shubhamraj2202@gmail.com>
2023-01-12 22:07:15 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""Package Release"""
+
+from __future__ import annotations
+
+from setuptools import find_packages, setup
+
+setup(
+    name="pyhelper-misc",
+    version="1.0.0",
+    description="Python Library Containing Generic Utilities and Helper Functions",
+    long_description=open("README.md", encoding="utf-8").read(),
+    author="Shubham Raj",
+    author_email="shubhamraj2202@gmail.com",
+    url="https://www.linkedin.com/in/shubhamraj2202/",
+    packages=find_packages(include=["py_helper", "py_helper.*"]),
+    install_requires=[],
+    tests_require=["pytest"],
+)
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install PipEnv
+python3 -m pip install --user pipenv
+# Install Dependencies
+python3 -m pipenv install
+# Sync Dev Dependencies
+python3 -m pipenv sync -d
+# Enter Shell
+python3 -m pipenv shell
+# Install Pre-Commit
+python3 -m pre-commit install
