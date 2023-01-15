70102528100421cc3bafedc24fd29636d330ffb7
Shubham Raj <shubhamraj2202@gmail.com>
2023-01-15 11:13:02 +0900

    Update Setup  Dependencies

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ from setuptools import find_packages, setup
 
 setup(
     name="pytube-downloader",
-    version="1.0.0",
+    version="1.0.1",
     description="Wrapper over pytube python's package",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Shubham Raj",
     author_email="shubhamraj2202@gmail.com",
     url="https://github.com/shubhamraj2202/pytube-downloader",
-    packages=find_packages(include=["src", "src.*"]),
-    install_requires=[],
+    packages=find_packages(include=["ytube", "ytube.*"]),
+    install_requires=["pytube", "pyhelper-misc", "pydantic"],
     tests_require=["pytest"],
     project_urls={
         "Bug Reports": "https://github.com/shubhamraj2202/pytube-downloader/issues",
